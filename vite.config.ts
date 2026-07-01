60b083c0b4cef51efd0ca7f5129c47fc4ba2ca66
lizabeth <elizavetka.chizh@gmail.com>
2026-07-01 14:07:14 +0300

    chore: vitest + testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    // Enable global test methods like describe, it, and expect
+    globals: true,
+    css: true,
+    // Choose 'jsdom' for frontend components or 'node' for pure backend logic
+    environment: 'jsdom',
+    
+    // Setup file to execute before running tests (e.g., loading custom matchers)
+    setupFiles: './src/test/setup.ts',
+    
+    // Pattern to look for test files
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    
+    // Coverage configuration (optional)
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
